d2130f49d94750f0eb9fc54c7488e5e710869efd
Kyohei Uemura <uemue1989@gmail.com>
2014-06-01 16:25:09 +0900

    Improve add-user-view

--- app/views/add_user_view.rb
+++ app/views/add_user_view.rb
@@ -1,10 +1,10 @@
 class AddUserView < UIView
   attr_accessor :text_field, :add_button
-  
+
   def initWithFrame(frame)
     super
 
-    self.backgroundColor = UIColor.whiteColor
+    self.backgroundColor = UIColor.lightGrayColor
 
     self << @text_field = UITextField.new.tap do |tf|
       tf.placeholder = "user name"
@@ -12,15 +12,11 @@ class AddUserView < UIView
       tf.autocapitalizationType = UITextAutocapitalizationTypeNone
       tf.borderStyle = UITextBorderStyleRoundedRect
     end
-
-    self << @add_button = UIButton.buttonWithType(UIButtonTypeRoundedRect).tap do |btn|
-      btn.setTitle("add", forState:UIControlStateNormal)
-    end
   end
 
   def layoutSubviews
-    @text_field.frame = [[100, 100], [160, 26]]
-    @add_button.sizeToFit
-    @add_button.frame = [[100, 130], @add_button.frame.size]
+    @text_field.frame = [[self.frame.origin.x + 10,
+                          self.frame.origin.y + 74],
+                         [self.frame.size.width - 20, 26]]
   end
 end
