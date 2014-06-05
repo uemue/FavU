3fb75262796abfa32233a0bde0bb2c30504a82a9
Kyohei Uemura <uemue1989@gmail.com>
2014-06-06 00:04:41 +0900

    create Tweet model

---
+++ app/models/tweet.rb
@@ -0,0 +1,30 @@
+class Tweet
+  attr_reader :id, :reply_to, :text, :user, :favorited, :retweeted_by
+
+  def initialize(data)
+    if data["retweeted_status"]
+      fill_with_data(data["retweeted_status"])
+      @retweeted_by = data["user"]
+    else
+      fill_with_data(data)
+    end
+
+    self
+  end
+
+  def fill_with_data(data)
+    @id        = data["id_str"]
+    @reply_to  = data["in_reply_to_status_id_str"]
+    @text      = data["text"]
+    @user      = data["user"]
+    @favorited = data["favorited"]
+  end
+
+  def favorite
+    @favorited = true
+  end
+
+  def unfavorite
+    @favorited = false
+  end
+end
