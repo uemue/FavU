4c17eb7cb8677d6246a901e61ab452a44290d33a
Kyohei Uemura <uemue1989@gmail.com>
2014-06-10 00:57:21 +0900

    Improve webView

--- app/views/tweet_detail_view.rb
+++ app/views/tweet_detail_view.rb
@@ -6,6 +6,7 @@ class TweetDetailView < UIView
 
     self << @webView = UIWebView.alloc.initWithFrame(self.bounds).tap do |wv|
       wv.scrollView.contentInset = [0, 0, 64, 0]
+      wv.scalesPageToFit = true
     end
 
     self << @indicatorView = UIActivityIndicatorView.new.tap do |iv|
