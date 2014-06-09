e613824edca96c5b4dd016bd5452db0af04eaa0c
Kyohei Uemura <uemue1989@gmail.com>
2014-06-09 21:06:21 +0900

    create user model

---
+++ app/models/user.rb
@@ -0,0 +1,48 @@
+class User
+  attr_reader :screen_name, :name, :profile_image_url, :id
+
+  def self.userWithScreenName(screen_name)
+    user = self.alloc.initWithScreenName(screen_name)
+    return user
+  end
+
+  def initialize(data)
+    @screen_name       = data["screen_name"]
+    @name              = data["name"]
+    @profile_image_url = data["profile_image_url"]
+    @id                = data["id"]
+  end
+
+  def to_hash
+    return {
+      "screen_name" => @screen_name,
+      "name" => @name,
+      "profile_image_url" => @profile_image_url,
+      "id" => @id
+    }
+  end
+
+  def initWithScreenName(screen_name)
+    @screen_name = screen_name
+    client = STTwitterAPI.shared_client
+
+    unless client.userName
+      client.verifyCredentialsWithSuccessBlock(lambda{ |user_name|
+        initWithScreenName(screen_name)
+      }, errorBlock: lambda{ |error|
+        UIAlertView.alert("Error", error.localizedDescription)
+      })
+      return self
+    end
+
+    client.get_user_information_for(@screen_name, successBlock: lambda{|data|
+      @screen_name       = data["screen_name"]
+      @name              = data["name"]
+      @profile_image_url = data["profile_image_url"]
+      @id                = data["id"]
+    }, errorBlock:lambda{|error|
+      UIAlertView.alert("Error", error.localizedDescription)
+    })
+    return self
+  end
+end
