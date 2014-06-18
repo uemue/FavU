378c4e17088993bca219fca7f27d88013b28f6f7
Kyohei Uemura <uemue1989@gmail.com>
2014-06-18 22:58:42 +0900

    Improve TextField in AddUserView
    fix #10

--- app/views/add_user_view.rb
+++ app/views/add_user_view.rb
@@ -4,13 +4,14 @@ class AddUserView < UIView
   def initWithFrame(frame)
     super
 
-    self.backgroundColor = UIColor.lightGrayColor
+    self.backgroundColor = "#CCCCCC".uicolor
 
     self << @text_field = UITextField.new.tap do |tf|
       tf.placeholder = "user name"
-      tf.textAlignment = UITextAlignmentCenter
+      tf.textAlignment = NSTextAlignmentLeft
       tf.autocapitalizationType = UITextAutocapitalizationTypeNone
       tf.borderStyle = UITextBorderStyleRoundedRect
+      tf.clearButtonMode = UITextFieldViewModeAlways
     end
   end
 
